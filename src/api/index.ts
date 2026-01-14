8d93bd96674059199de5548212129b1ff92fbb30
Ashish M <ashish@ashish.org>
2026-01-13 20:41:30 -0800

    feat: Export all new API functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,3 @@
-export * from './tasks'
-export * from './lists'
-export * from './scheduled'
+export { getTasks, createTask, updateTask, deleteTask, completeTask, uncompleteTask, moveToPurgatory, moveFromPurgatory, spawnDailyTasks } from './tasks'
+export { getLists, createList, updateList, deleteList, duplicateList, ensureTodayList } from './lists'
+export { getScheduledTasks, scheduleTask, unscheduleTask, updateScheduleTime } from './scheduled'
